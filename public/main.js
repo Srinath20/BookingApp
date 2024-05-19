aa809b571605c8a050fff254654770d631f400f4
Srinath20 <srinathgollapinni>
2024-05-19 07:45:40 +0530

    Added .gitignore

---
+++ public/main.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', function () {
+  fetchAppointments();
+});
+
+function addAppointment() {
+  var username = document.getElementById('username').value;
+  var email = document.getElementById('email').value;
+
+  if (username && email) {
+      var appointmentData = { username: username, email: email };
+      axios.post('http://localhost:3000/api/users', appointmentData)
+          .then(function (response) {
+              console.log(response.data);
+              document.getElementById('username').value = '';
+              document.getElementById('email').value = '';
+              fetchAppointments();
+          })
+          .catch(function (error) {
+              console.error('Error adding appointment:', error);
+          });
+  }
+}
+
+function fetchAppointments() {
+  axios.get('http://localhost:3000/api/users')
+      .then(function (response) {
+          displayAppointments(response.data);
+      })
+      .catch(function (error) {
+          console.error('Error retrieving appointments:', error);
+      });
+}
+
+function displayAppointments(appointments) {
+  var appointmentList = document.getElementById('appointmentList');
+  appointmentList.innerHTML = '';
+  appointments.forEach(function (appointment) {
+      var appointmentItem = document.createElement('div');
+      appointmentItem.innerHTML = `
+          <p>Username: ${appointment.username}</p>
+          <p>Email: ${appointment.email}</p>
+          <button onclick="deleteUser(${appointment.id})">Delete</button>
+      `;
+      appointmentList.appendChild(appointmentItem);
+  });
+}
+
+function deleteUser(id) {
+  axios.delete(`http://localhost:3000/api/users/${id}`)
+      .then(function (response) {
+          console.log('User deleted:', response.data);
+          fetchAppointments();
+      })
+      .catch(function (error) {
+          console.error('Error deleting user:', error);
+      });
+}
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const port = 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('public'));
 
 app.use('/api/users', userRoutes);
 
